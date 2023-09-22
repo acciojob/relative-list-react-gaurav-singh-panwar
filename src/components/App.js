ab584cca8cfe02ee4891abb170d4c9563108bc47
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-09-22 18:11:59 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+let list = ['ram', 'shyam', 'hari', 'bhole', 'nath', 'gopal', 'krishn', 'kanha', 'shankar'];
+
 const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol id='relativeList'>
+                {
+                  list.map((name, indx)=>(
+                    <li id={`relativeListItem${indx+1}`}>{name}</li>
+                  ))
+                }
+               </ol>
     </div>
   )
 }
